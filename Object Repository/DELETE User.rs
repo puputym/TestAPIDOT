17cc6d0f842da1a10e6219a5a470212b03ef8486
puputym <puput@upi.edu>
2022-11-19 07:12:40 +0700

    first upload API test at DOT

---
+++ Object Repository/DELETE User.rs
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>DELETE User</name>
+   <tag></tag>
+   <elementGuidId>59d09f77-5528-4254-9b48-a6835240c915</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>-1</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.2.5</katalonVersion>
+   <maxResponseSize>-1</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>DELETE</restRequestMethod>
+   <restUrl>https://reqres.in/api/users/2</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>-1</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+
+WS.verifyResponseStatusCode(response, 204)
+
+assertThat(response.getStatusCode()).isEqualTo(204)
+
+
+assertThat(response.getStatusCode()).isIn(Arrays.asList(200, 201, 202, 204))</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/Result Test API.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Result Test API</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ee600143-37cf-4802-9807-930603a465a0</testSuiteGuid>
+   <testCaseLink>
+      <guid>fac757f8-3ff8-47b4-86c8-354c54946197</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01- Verify Get All User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8da987a8-c5be-4de9-89f1-5549f31bd5db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01-Verify Post Create User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>917a13ce-d8cc-4a88-a773-b9ba37c88c58</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03 - Verify Post Register Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>98f6e990-0c22-4827-9160-869fb9a251dd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04 - veirfy Post Login Success</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9291666b-1fdf-4219-8558-d8cd3de026e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC05 - Verify Put Update User</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c60cf27f-6ca9-4e78-a131-186a72986925</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC06 - Verify Delete User</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
